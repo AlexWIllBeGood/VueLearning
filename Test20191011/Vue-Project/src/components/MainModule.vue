0e4de1c37fc9fe43ebfaa074071619ffdeead1ff
AlexWillBeGood <AlexWillBeGood@163.com>
2019-10-11 18:26:23 +0800

    common commit

--- Test20191011/Vue-Project/src/components/MainModule.vue
+++ Test20191011/Vue-Project/src/components/MainModule.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="MainModule">
         <p>get value from SubModule</p>
-        <SubModule @valueChange="subValueChange" ></SubModule>
+        <SubModule @valueChange="subValueChange" :lessonTypeList="lessonTypeList" :visible.sync="childShow"></SubModule>
         <el-button @click="showSub">show</el-button>
     </div>
 </template>
@@ -12,7 +12,29 @@ export default {
     data:function(){
         return{
             lessonType:"",
-            childShow:false
+            childShow:true,
+            lessonTypeList:[
+                {
+                    id:1,
+                    enName:"StudyWord",
+                    name:"单词学习"
+                },
+                {
+                    id:2,
+                    enName:"StudySyntax",
+                    name:"句型学习"
+                },
+                {
+                    id:3,
+                    enName:"StudyDialogue",
+                    name:"对话学习"
+                },
+                {
+                    id:4,
+                    enName:"TestParam",
+                    name:"测试传参"
+                }
+            ]
         }
     },
     methods:{
@@ -20,11 +42,16 @@ export default {
             this.lessonType=e;
         },
         showSub:function(){
-
+            this.childShow=true;
         }
     },
     components:{
         SubModule
+    },
+    watch:{
+        childShow:function(nval,oval){
+            console.log(`from ${oval} to ${nval}`);
+        }
     }
 }
 </script>
